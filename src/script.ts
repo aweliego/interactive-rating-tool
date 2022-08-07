e235d2e58ce77943252afe1833fcbcb47816a67e
aweliego <amelie.gorry.am@gmail.com>
2022-08-07 21:43:23 +0200

    - style hover and active states for scores and submit btn - enable style toggling via JS - store selected score

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,36 @@
+const scores = document.querySelectorAll(
+  '.score'
+) as NodeListOf<HTMLElement> | null;
+const submitBtn = document.querySelector<HTMLButtonElement>('#submit-btn');
 
+submitBtn?.addEventListener('click', () => {
+  // set main to ''
+  // create thank-you component (article element, append to main)
+  // string interpolation using getScore()
+
+});
+
+const toggleColor = (score: any): void => {
+  removeActiveClasses();
+  score.classList.add('active');
+};
+
+const removeActiveClasses = () => {
+  scores?.forEach((score) => score.classList.remove('active'));
+};
+
+const getScore = (score: any) => {
+  const selectedScore = score.innerHTML;
+  console.log(selectedScore);
+  return selectedScore;
+};
+
+
+if (scores != null) {
+  scores.forEach((score) => {
+    score.addEventListener('click', () => {
+      toggleColor(score);
+      getScore(score);
+    });
+  });
+}
