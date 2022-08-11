0b687abbb4089d616b28a4e243284b52be7bf5ff
aweliego <amelie.gorry.am@gmail.com>
2022-08-11 09:17:34 +0200

    feature: show selected score in thank you component - show clicked score in orange - on submit, replace rating tool by thank you component - show selected score in thank you component

--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,41 @@
 const scores = document.querySelectorAll(
   '.score'
 ) as NodeListOf<HTMLElement> | null;
+
 const submitBtn = document.querySelector<HTMLButtonElement>('#submit-btn');
 
-submitBtn?.addEventListener('click', () => {
-  // set main to ''
-  // create thank-you component (article element, append to main)
-  // string interpolation using getScore()
+const main = document.getElementById('main');
 
+let selectedScore: string;
+
+if (scores != null) {
+  scores.forEach((score) => {
+    score.addEventListener('click', () => {
+      selectedScore = score.innerHTML;
+    });
+  });
+}
+
+submitBtn?.addEventListener('click', () => {
+  if (main != null) {
+    main.innerHTML = '';
+    const article = document.createElement('article');
+    article.classList.add('thank-you');
+    article.innerHTML = `<header>
+    <img src="../images/illustration-thank-you.svg" alt="" /> <br />
+    <p class="given-score">You selected ${selectedScore} out of 5</p>
+
+    <h2>Thank you!</h2>
+  </header>
+  <section>
+    <p class="thank-you-text">
+      We appreciate you taking the time to give a rating. If you ever need
+      more support, don’t hesitate to get in touch!
+    </p>
+  </section>`;
+
+    main.append(article);
+  }
 });
 
 const toggleColor = (score: any): void => {
@@ -19,18 +47,10 @@ const removeActiveClasses = () => {
   scores?.forEach((score) => score.classList.remove('active'));
 };
 
-const getScore = (score: any) => {
-  const selectedScore = score.innerHTML;
-  console.log(selectedScore);
-  return selectedScore;
-};
-
-
 if (scores != null) {
   scores.forEach((score) => {
     score.addEventListener('click', () => {
       toggleColor(score);
-      getScore(score);
     });
   });
 }
